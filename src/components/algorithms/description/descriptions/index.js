f4e7ac4fa4b314a0e5056bfca7c0422f09711008
dkblock <wadeouter@gmail.com>
2021-11-19 14:27:50 +0300

    Remove descriptions

--- src/components/algorithms/description/descriptions/index.js
+++
@@ -1,9 +0,0 @@
-import AvlTreeDescription from "./avl-tree-description";
-import RedBlackTreeDescription from "./red-black-tree-description";
-
-const algorithmDescriptions = {
-  "avl-tree": AvlTreeDescription,
-  "red-black-tree": RedBlackTreeDescription,
-};
-
-export default algorithmDescriptions;
